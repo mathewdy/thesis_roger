b45868accfd3f3ce2bccf12c02d33d00a4a10ca4
Raie01 <ryantecling@gmail.com>
2023-04-15 07:16:26 +0800

    Update book.php

--- users/book.php
+++ users/book.php
@@ -39,8 +39,8 @@ session_start();
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo mr-5" href="index.php">HBMS</a>
-            <a class="navbar-brand brand-logo-mini" href="index.php">HBMS</a>
+            <a class="navbar-brand brand-logo mr-5" href="../index.php">HBMS</a>
+            <a class="navbar-brand brand-logo-mini" href="../index.php">HBMS</a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
             <ul class="navbar-nav navbar-nav-right">
